cfbeecab43e32ea0767872e177ba269576812da2
Farouk Goaudria <gouadriaafar@gmail.com>
2021-10-22 00:23:46 +0100

    install bootstrap and create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
-
+import {Route ,Switch,Redirect}from "react-router-dom";
+import Home from "./components/Home"
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Product from "./components/Product"; 
 function App() {
   return (
     <>
-    CarShow
+    <Switch>
+      <Route exact to="/" component={ Home } />
+      <Route exact to="/about" component={ About } />
+      <Route exact to="/contact" component={ Contact } />
+      <Route exact to="/Products" component={ Product } />
+
+    </Switch>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import {BrowserRouter as Router} from "react-router-dom"
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
